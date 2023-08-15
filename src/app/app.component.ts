57725dbde5190d154b026e90c0f98440e43b3978
pc <rahimjangi1982@gmail.com>
2023-08-15 18:23:18 +0200

    Client init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  urlPath = 'https://localhost:5000/api/AppUsers';
   title = 'client';
+  users: any;
+  constructor(private _httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this._httpClient.get(this.urlPath).subscribe({
+      next: (response) => {
+        this.users = response;
+        console.log(response);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
