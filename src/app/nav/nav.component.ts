1eb1927bf4fd2cacd4d996ff788830aac0ca22ad
pc <rahimjangi1982@gmail.com>
2023-08-18 17:50:26 +0200

    Angular Routing Done!

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -11,18 +13,19 @@ import { User } from '../_models/user';
 export class NavComponent implements OnInit {
   model: any = {};
   // currentUser$: Observable<User | null> = of(null);
-  constructor(public accountService: AccountService) {}
+  constructor(public accountService: AccountService,private router:Router,private toastr:ToastrService) {}
   ngOnInit(): void {
     // this.currentUser$ = this.accountService.currentUser$;
   }
   login() {
     this.accountService.login(this.model).subscribe({
       next: (val) => {
-        console.log(val);
-        
+        this.router.navigateByUrl("/members");
+        this.toastr.success("Welcome on board!", "Success");
       },
       error: (err) => {
         console.log(err);
+        this.toastr.error(err.error, "Login failed!");
       },
       complete: () => {
         console.log('login service completed!');
@@ -33,6 +36,7 @@ export class NavComponent implements OnInit {
 
   Logout() {
     this.accountService.logout();
+    this.router.navigateByUrl("/");
     
   }
 
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -10,16 +11,19 @@ export class RegisterComponent {
   model: any = {};
   @Output() cancelRegister = new EventEmitter();
 
-  constructor(private accountService: AccountService) {}
+  constructor(private accountService: AccountService,private toastr:ToastrService) {}
 
   register() {
     this.accountService.register(this.model).subscribe({
       next: (user) => {
         console.log(user);
         this.cancel();
+        this.toastr.success("Welcom on board!", "Registeration successfull");
       },
       error: (err) => {
         console.log(err);
+        this.toastr.error(err.error, "Failed to register!");
+
       },
       complete: () => {
         console.log('register completed!');
